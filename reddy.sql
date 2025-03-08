143f46be25f30a7da62c495f85772f50d88ff70b
srinu1622 <r95310737@gmail.com>
2025-03-08 12:08:11 +0530

    Add files via upload

---
+++ reddy.sql
@@ -0,0 +1,18 @@
+--select upper('data'), lower('data'), length('data')
+--select initcap('data engineering')
+--select ltrim('  data  '), rtrim('  data  ') from dual
+
+
+ 
+--select lpad('123',5,'0') from dual
+--select rpad('1234656',8,'0')from dual
+--select trim('  data  ') from dual
+--select replace ('data scince','science','engineering') from dual
+--select abs(-10) as absolute_value from dual
+--select floor(12.45) from dual
+--select round(12.345,2) as rounded_value from dual
+--select mod(10,3) as reminder from dual
+--select log(10, 1000) as log_value from dual
+--select greatest(10,20,30,40) from dual
+--select least(10,20,30,40) from dual
+select sin(0),cos(0),tan(0),atan(0),acos(0),asin(0), from dual
